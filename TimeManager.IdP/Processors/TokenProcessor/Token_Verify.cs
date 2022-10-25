72a709c34c0d6608ff1d6ee48c6e5fc1a9b9fbdc
Arkadiusz Staśko <arek.stasko@gmail.com>
2022-10-25 22:00:51 +0200

    Change return form of token verify

--- TimeManager.IdP/Processors/TokenProcessor/Token_Verify.cs
+++ TimeManager.IdP/Processors/TokenProcessor/Token_Verify.cs
@@ -49,7 +49,7 @@ namespace TimeManager.IdP.Processors.TokenProcessor
             catch (Exception ex)
             {
                 _logger.LogError(ex.Message);
-                throw new Exception(ex.Message);
+                return -1; 
             }
             
         }                                                     
